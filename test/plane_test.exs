4945c8d13e3f1417e068f82fc3f62d674445f8e4
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2017-04-25 18:49:35 +0200

    Correct testing

--- test/plane_test.exs
+++ test/plane_test.exs
@@ -1,21 +1,27 @@
 defmodule PlaneTest do
   use ExUnit.Case
   use Plane
+
+  setup do
+    on_exit fn ->
+      File.rm_rf("testdb")
+    end
+  end
   
   test "A database can be created" do
-    with_level "testdb1" do
+    with_level "testdb" do
       assert Exleveldb.is_empty?(db)
     end
   end
 
   test "A database can be destroyed" do
-    with_level "testdb2" do
-      Exleveldb.destroy "testdb2"
+    with_level "testdb" do
+      Exleveldb.destroy "testdb"
     end
   end
 
   test "A database is closed when leaving the with_level scope" do
-    with_level "testdb3" do
+    with_level "testdb" do
       :stuff_happens_in_here
     end
     
@@ -23,6 +29,4 @@ defmodule PlaneTest do
       Exleveldb.close(db)
     end
   end
-
-  for x <- ["testdb1", "testdb2", "testdb3"], do: Exleveldb.destroy(x)
 end
